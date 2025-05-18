3e40ebff65b1372647e8041879073a60f1acce84
Ridouane-ridix <ridix0202@gmail.com>
2025-05-18 18:18:10 +0100

    C02

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,13 +1,21 @@
-﻿#include <stdio.h>
-int main(){
-    int a, b;
-    //lecteur des deux nombre
+#include <stdio.h>
+
+int main() {
+ int a, b;
+
+    // Lire les deux nombres
+    printf("Entrez deux nombres : ");
     scanf("%d %d", &a, &b);
-    //Echanger les valeurs sans variable temporaire
-    a = a + b; // a devient la somme de a et b
-    b = a - b; // b devient l'ancienne valeur de a
-    a = a - b; // a devient l'ancienne valeur de b
-    //Affichage des valeurs àprés l'echange
-    printf("Après échange: a = %d, b = %d\n", a, b);
+
+    // Afficher avant l'échange
+    printf("Avant échange : a = %d, b = %d\n", a, b);
+
+    // Échanger les nombres sans variable temporaire (méthode arithmétique)
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    // Afficher après l'échange
+    printf("Après échange : a = %d, b = %d\n", a, b);
     return 0;
 }
