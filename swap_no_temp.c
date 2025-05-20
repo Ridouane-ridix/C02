c6d3b69be029a672003026a129424cacdd690636
Ridouane-ridix <ridix0202@gmail.com>
2025-05-20 17:03:09 -0400

    Add files via upload

---
+++ swap_no_temp.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main() {
+
+    int a, b; //declaration de deux variables entiers
+    
+    scanf("%d %d", &a, &b);  //lecteur des deux nombre
+
+    
+    a = a + b;    // a devient la somme de a et b
+    b = a - b;   // b devient l'ancienne valeur de a
+    a = a - b;  // a devient l'ancienne valeur de b
+     
+    printf("Après échange: a = %d, b = %d\n", a, b); //Affichage des valeurs àprés l'echange
+
+    return 0; //fin du programme
+}
